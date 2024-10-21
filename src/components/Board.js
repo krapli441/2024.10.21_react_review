eccf359e183412574683bbc9cc5661a2b41025df
unknown <tlfqj1208@gmail.com>
2024-10-21 16:39:21 +0900

    square 로직 수정, 부모의 props 받아오도록 함

--- src/components/Board.js
+++ src/components/Board.js
@@ -9,8 +9,22 @@ class Board extends Component {
       squares: Array(9).fill(null),
     };
   }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = "X";
+    this.setState({ squares: squares });
+  }
+
   renderSquare(i) {
-    return <Square value={this.state.squares[i]} />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => {
+          this.handleClick(i);
+        }}
+      />
+    );
   }
 
   render() {
--- src/components/Square.js
+++ src/components/Square.js
@@ -4,7 +4,12 @@ import "./square.css";
 class Square extends Component {
   render() {
     return (
-      <button className="square" onClick={this.props.onClick}>
+      <button
+        className="square"
+        onClick={() => {
+          this.props.onClick();
+        }}
+      >
         {this.props.value}
       </button>
     );
