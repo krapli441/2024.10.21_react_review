ab3ec16da0ac1ec7521177a5471c785e3e9dd18e
unknown <tlfqj1208@gmail.com>
2024-10-21 13:50:09 +0900

    기초 div 작성

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="game">
+      <div className="game-board"></div>
+      <div className="game-info">
+        <div> status </div>
+        <div> TODO </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
