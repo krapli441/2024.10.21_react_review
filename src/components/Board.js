257ff538941367bb446e48ee583b07f35769a2c8
unknown <tlfqj1208@gmail.com>
2024-10-21 18:53:41 +0900

    status 및 winnerCalculator 추가하겨 승자 확인 로직 추가

--- src/components/Board.js
+++ src/components/Board.js
@@ -5,8 +5,37 @@ import "./board.css";
 const Board = () => {
   const [squares, setSquares] = useState(Array(9).fill(null));
   const [xIsNext, setXIsNext] = useState(true);
-  const status = `Next Player: ${xIsNext ? "X" : "O"}`;
 
+  const calculateWinner = (squares) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+  };
+
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner : " + winner;
+  } else {
+    status = "Next player :" + (xIsNext ? "X" : "O");
+  }
   const handleClick = (i) => {
     const newSquares = squares.slice();
     newSquares[i] = xIsNext ? "O" : "X";
