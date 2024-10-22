af5c8dd973533faa20ab1b1c7b56b7ee455ba5d6
unknown <tlfqj1208@gmail.com>
2024-10-22 11:11:47 +0900

    board.js 로직을 app.js로 우선 변경, board.js 수정 필요

--- src/App.js
+++ src/App.js
@@ -3,7 +3,51 @@ import Board from "./components/board";
 import { useState } from "react";
 
 const App = () => {
-  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [history, setHistory] = useState([{ squares: Array(9).fill(null) }]);
+  const [xIsNext, setXIsNext] = useState(true);
+
+  const calculateWinner = (squares) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+  };
+
+  const current = history[history.length - 1];
+  const winner = calculateWinner(current.squares);
+
+  let status;
+  if (winner) {
+    status = "Winner : " + winner;
+  } else {
+    status = "Next player :" + (xIsNext ? "X" : "O");
+  }
+
+  const handleClick = (i) => {
+    const newSquares = current.squares.slice();
+    if(calculateWinner(newSquares) || newSquares[i]) {
+      return;
+  } else {
+    newSquares[i] = xIsNext? "X" : "O";
+    setHistory([...history, {squares: newSquares}]);
+    setXIsNext(prev => !prev);
+  }
 
   return (
     <div className="game">
--- src/components/Board.js
+++ src/components/Board.js
@@ -3,49 +3,16 @@ import Square from "./square";
 import "./board.css";
 
 const Board = () => {
-  const [squares, setSquares] = useState(Array(9).fill(null));
-  const [xIsNext, setXIsNext] = useState(true);
-
-  const calculateWinner = (squares) => {
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
-    for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];
-      if (
-        squares[a] &&
-        squares[a] === squares[b] &&
-        squares[a] === squares[c]
-      ) {
-        return squares[a];
-      }
-    }
-  };
-
-  const winner = calculateWinner(squares);
-  let status;
-  if (winner) {
-    status = "Winner : " + winner;
-  } else {
-    status = "Next player :" + (xIsNext ? "X" : "O");
-  }
-  const handleClick = (i) => {
-    const newSquares = squares.slice();
-    setSquares(newSquares);
-    setXIsNext(!xIsNext);
-    if (calculateWinner(newSquares) || newSquares[i]) {
-      return;
-    }
-    newSquares[i] = xIsNext ? "O" : "X";
-    setSquares(newSquares);
-  };
+  // const handleClick = (i) => {
+  //   const newSquares = squares.slice();
+  //   setSquares(newSquares);
+  //   setXIsNext(!xIsNext);
+  //   if (calculateWinner(newSquares) || newSquares[i]) {
+  //     return;
+  //   }
+  //   newSquares[i] = xIsNext ? "O" : "X";
+  //   setSquares(newSquares);
+  // };
 
   const renderSquare = (i) => {
     return (
