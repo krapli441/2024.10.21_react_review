da47e6d213660d437b722467b16aed16cdea0394
박준형 <tlfqj1208@gmail.com>
2024-10-24 10:27:06 +0900

    App.js 기초 코드 작성 - fetch 문구

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import { useState, useEffect } from "react";
+
 function App() {
-  return <div className="App"></div>;
+  const [value, setValue] = useState("");
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((data) => setPosts(data));
+  }, []);
+
+  return (
+    <div
+      className="App"
+      style={{
+        padding: "1rem",
+      }}
+    >
+      <input value={value} onChange={(e) => setValue(e.target.value)}></input>
+    </div>
+  );
 }
 
 export default App;
